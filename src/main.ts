0086e554bd744ecc1453dcd47711dc5df8fa5edf
VishalChouhan17 <vishal.chouhan2028@gmail.com>
2026-09-06 09:31:11 +0530

    added port for production

--- src/main.ts
+++ src/main.ts
@@ -25,9 +25,10 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 
-  await app.listen(3000);
-  console.log(`Application running on: http://localhost:3000`);
-  console.log(`Swagger Docs available at: http://localhost:3000/api`);
+  console.log(`🚀 Application running on: http://localhost:${port}`);
+  console.log(`📚 Swagger Docs available at: http://localhost:${port}/api`);
 }
 bootstrap();
